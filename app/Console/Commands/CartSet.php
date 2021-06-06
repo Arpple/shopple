7e6f1e0084208a5ea45399b703f69f11cd8acc5e
arpple <apple0239@gmail.com>
2021-06-07 00:45:58 +0700

    cart set

---
+++ app/Console/Commands/CartSet.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Core\Checkout\Domain\CartService;
+use App\Core\User\Domain\UserService;
+use Illuminate\Console\Command;
+
+class CartSet extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cart:set {username} {productId} {quantity}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'View current cart';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $username = $this->argument('username');
+        $user = (new UserService)->login($username);
+
+        if (is_null($user)) {
+            echo 'user not found';
+            return 1;
+        }
+
+        $productId = $this->argument('productId');
+        $quantity = $this->argument('quantity');
+
+        $cart = (new CartService($user->id))
+            ->setProductItem($productId, $quantity)
+            ->get();
+
+        $result = (new CartView($user, $cart))
+            ->toString();
+
+        echo $result . PHP_EOL;
+
+        return 0;
+    }
+}
