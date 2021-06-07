c6472c92119ac927b464980a29cffc0422b73d2c
arpple <apple0239@gmail.com>
2021-06-08 01:59:12 +0700

    test

--- app/Console/Checkout/CartViewTest.php
+++ app/Console/Checkout/CartViewTest.php
@@ -3,6 +3,7 @@
 namespace App\Console\Checkout;
 
 use App\Core\Checkout\Domain\CartEntity;
+use App\Core\Checkout\Test\Example;
 use App\Core\User\Domain\UserEntity;
 use PHPUnit\Framework\TestCase;
 
@@ -19,6 +20,26 @@ class CartViewTest extends TestCase
         $expect = implode(PHP_EOL, [
             'arpple\'s cart',
             '$0',
+        ]);
+
+        $this->assertEquals($expect, $str);
+    }
+
+    public function test_print_cart_with_items()
+    {
+        $user = new UserEntity(1, 'arpple');
+        $cart = (new CartEntity)
+            ->addItem(Example::itemA())
+            ->addItem(Example::itemB());
+
+        $str = (new CartView($user, $cart))
+            ->toString();
+
+        $expect = implode(PHP_EOL, [
+            'arpple\'s cart',
+            '  - A 100x1 = $100',
+            '  - B 200x2 = $400',
+            '$500',
         ]);
 
         $this->assertEquals($expect, $str);
